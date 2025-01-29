ebaac3168d24f19b983e7d1f6c49eda0e166a04c
Bernadetta Majta <bernadettamajta@gmail.com>
2025-01-29 01:55:32 +0100

    I added documentation for the function palindrom

--- palindromy.py
+++ palindromy.py
@@ -1,4 +1,9 @@
 def palindrom(s):
+    """
+    Checks if argument passed is a palindrom, prints boolean value of this check
+    Argument:
+    s
+    """
     s = ''.join(znak.lower() for znak in str(s) if znak.isalnum())
     return s == s[::-1]
 print(palindrom("kajak"))
