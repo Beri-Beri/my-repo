27ae71928dbb2265d5f437888b683c032387fa25
Bernadetta Majta <bernadettamajta@gmail.com>
2025-02-04 21:37:16 +0100

    Empty functions added

--- kalkulator.py
+++ kalkulator.py
@@ -1,2 +1,15 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+def get_numbers():
+    pass
+def suma(numbers):
+    pass
+def minus(numbers):
+    pass
+def div(numbers):
+    pass
+def multi(numbers):
+    pass
+if __name__ == "__main__":
+    pass
