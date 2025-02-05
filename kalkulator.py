c3d2b7b9f2bb92b31d84acc9636f7b56ba0bd6aa
Bernadetta Majta <bernadettamajta@gmail.com>
2025-02-05 17:27:47 +0100

    Added get_numbers function to collect user input

--- kalkulator.py
+++ kalkulator.py
@@ -13,3 +13,23 @@ def multi(numbers):
     pass
 if __name__ == "__main__":
     pass
+
+def get_numbers():
+    numbers = []
+    index = 1
+    while len(numbers) < 2:
+        try:
+            num = input(f"Podaj składnik {index}. (lub wpisz 'q', aby zakończyć): ")
+            if num.lower() == 'q':
+                if len(numbers) < 2:
+                    quiet_logger.warning("Musisz podać przynajmniej dwie liczby!")
+                    continue
+                break
+            numbers.append(float(num))
+            quiet_logger.info(f"Podaj składnik {index}: {num}")
+            index += 1
+        except ValueError:
+            choice = input("To nie jest liczba. Wpisz 'q', aby zakończyć lub spróbuj ponownie: ")
+            if choice.lower() == 'q':
+                break
+    return numbers
