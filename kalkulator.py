d6c1965e5854ecc5d758f2055f5068ba5a830636
Bernadetta Majta <bernadettamajta@gmail.com>
2025-02-05 19:16:15 +0100

    Add script logic for math operations with user input

--- kalkulator.py
+++ kalkulator.py
@@ -33,3 +33,23 @@ def get_numbers():
             if choice.lower() == 'q':
                 break
     return numbers
+
+    if __name__ == "__main__":
+    operation = input(f"Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+
+    numbers = get_numbers()
+
+    if operation == '1':
+        print(f"Wynik: {suma(numbers)}")
+    elif operation == '2':
+        print(f"Wynik: {minus(numbers)}")
+    elif operation == '3':
+        print(f"Wynik: {multi(numbers)}")
+    elif operation == '4':
+        try:
+            print(f"Wynik: {div(numbers)}")
+        except ZeroDivisionError as x:
+            print(f"Błąd: {x}")
+    else:
+        logging.error("Niepoprawna operacja!")
+        print("Niepoprawna operacja")
