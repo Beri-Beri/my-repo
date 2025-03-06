0f1c94c3652259fde090840db8ddc93aa2d1bb69
Bernadetta Majta <bernadettamajta@gmail.com>
2025-03-06 18:04:05 +0100

    feat: add __str__ method to Movie and Series

--- Biblioteka_filmow.py
+++ Biblioteka_filmow.py
@@ -7,6 +7,9 @@ class Movie:
         #Variable
         self._current_views=0
 
+    def __str__(self):
+        return f"{self.title} ({self.release})"
+
 class Series(Movie):
     def __init__(self, title, release, genre, ep_no, season_no):
         super().__init__(title, release, genre)
@@ -15,3 +18,7 @@ class Series(Movie):
 
          #Variable
         self._current_views = 0
+    
+    def __str__(self):
+        return f"{self.title} S{self.season_no:02d}E{self.ep_no:02d}"
+    
