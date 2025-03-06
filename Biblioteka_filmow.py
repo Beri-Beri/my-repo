f7f53d1a7707606449f741c004cd5f0b0a9a016e
Bernadetta Majta <bernadettamajta@gmail.com>
2025-03-06 21:17:47 +0100

    feat: implement play method and view counter

--- Biblioteka_filmow.py
+++ Biblioteka_filmow.py
@@ -9,6 +9,20 @@ class Movie:
 
     def __str__(self):
         return f"{self.title} ({self.release})"
+    
+    def play(self, viewed=1):
+        self._current_views += viewed
+
+    @property
+    def current_views(self):
+        return self._current_views
+    
+    @current_views.setter
+    def current_views(self, value):
+        if value >= self._current_views:
+            self._current_views = value
+        else:
+            raise ValueError("Nie można zmniejszyć liczby wyświetleń")
 
 class Series(Movie):
     def __init__(self, title, release, genre, ep_no, season_no):
