cb24f34c671552e4cdd09aa6e2453c899f6f66ef
Bernadetta Majta <bernadettamajta@gmail.com>
2025-03-08 19:14:19 +0100

    init: create BusinessCard class

--- wizytowka_task.py
+++ wizytowka_task.py
@@ -1,2 +1,12 @@
 from faker import Faker
 
+class BusinessCard:
+    def __init__(self, name, last_name, job_title, company, phone, email)
+        self.name = name
+        self.last_name = last_name
+        self.job_title = job_title
+        self.company = company
+        self.phone = phone
+        self.email = email
+
+    
