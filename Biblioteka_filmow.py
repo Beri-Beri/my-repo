d842fdb26787f7376962c55c6bd5b06e79770f30
Bernadetta Majta <bernadettamajta@gmail.com>
2025-03-08 13:37:54 +0100

    feat: implement movie library with add and generate functions

--- Biblioteka_filmow.py
+++ Biblioteka_filmow.py
@@ -36,3 +36,22 @@ class Series(Movie):
     def __str__(self):
         return f"{self.title} S{self.season_no:02d}E{self.ep_no:02d}"
     
+library = []
+
+def add_to_library(item):
+    if isinstance(item, (Movie, Series)):
+        library.append(item)
+    else:
+        print("Element, który chcesz dodać nie jest filmem lub serialem")
+
+def generate_library():
+    #movies_ex
+    add_to_library(Movie("Inception", 2010, "Sci-Fi"))
+    add_to_library(Movie("The Matrix", 1999, "Sci-Fi"))
+    add_to_library(Movie("Avatar", 2009, "Sci-Fi"))
+    #series_ex
+    add_to_library(Series("Breaking Bad", 2008, "Drama", 1, 1))
+    add_to_library(Series("Breaking Bad", 2008, "Drama", 2, 1))
+    add_to_library(Series("Stranger Things", 2016, "Horror", 1, 1))
+    add_to_library(Series("Stranger Things", 2016, "Horror", 2, 1))
+
