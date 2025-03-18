1422be74d83154ace3e1e13ff95269fdef4c096c
Bernadetta Majta <bernadettamajta@gmail.com>
2025-03-19 00:44:56 +0100

    feat: add functions to delete all rows and delete with conditions

--- SQLite_w_praktyce.py
+++ SQLite_w_praktyce.py
@@ -76,3 +76,37 @@ def update(conn, table, id, **kwargs):
     except sqlite3.OperationalError as e:
        print(e)
 
+def delete_all(conn, table):
+     """
+   Delete all rows from table
+   :param conn: Connection to the SQLite database
+   :param table: table name
+   :return:
+   """
+     sql = f'DELETE FROM {table}'
+     cur = conn.cursor()
+     cur.execute(sql)
+     conn.commit()
+
+     print('Deleted')
+
+def delete_where(conn, table, **kwargs):
+    """
+   Delete from table where attributes from
+   :param conn:  Connection to the SQLite database
+   :param table: table name
+   :param kwargs: dict of attributes and values
+   :return:
+   """
+    qs = []
+    values = tuple()
+    for k, v in kwargs.items():
+        qs.append(f"{k}=?")
+        values += (v, )
+    q = " AND ".join(qs)
+
+    sql = f'DELETE FROM {table} WHERE {q}'
+    cur = conn.cursor()
+    cur.execute(sql, values)
+    conn.commit()
+    print("Deleted")
