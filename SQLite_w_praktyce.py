d7b09cd52ef9c14a7144227adc7e9dbcb52d3b41
Bernadetta Majta <bernadettamajta@gmail.com>
2025-03-18 23:52:26 +0100

    Implement function to create a SQLite database connection

--- SQLite_w_praktyce.py
+++ SQLite_w_praktyce.py
@@ -1,2 +1,13 @@
 import sqlite3
 from sqlite3 import Error
+
+def create_connection(db_file):
+   """ create a database connection to a SQLite database """
+   conn = None
+   try:
+       conn = sqlite3.connect(db_file)
+       print(f"Connected to {db_file}, sqlite version: {sqlite3.sqlite_version}")
+       return conn
+   except Error as e:
+       print(e)
+   return conn
