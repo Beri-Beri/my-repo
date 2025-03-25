eb773dd0a049551d465035a44ed569961bcd3b4b
Bernadetta Majta <bernadettamajta@gmail.com>
2025-03-26 00:58:51 +0100

    Add clean_stations and clean_measure tables

--- Module_6/Baza_danych.py
+++ Module_6/Baza_danych.py
@@ -7,3 +7,25 @@ engine = create_engine('sqlite:///data.db')
 
 metadata = MetaData()
 
+clean_stations = Table(
+    'clean_stations', metadata,
+    Column('station', String(11), primary_key=True),
+    Column('latitude', Float),
+    Column('longitude', Float),
+    Column('elevation', Float),
+    Column('name', String(255) ),
+    Column('country', String(255)),
+    Column('state', String(255))
+)
+
+clean_measure = Table(
+    'clean_measure', metadata,
+    Column('station', String(11), ForeignKey('clean_stations.station'), primary_key=True),
+    Column('date', Date, primary_key=True),
+    Column('precip', Float),
+    Column('tobs', Float)
+)
+
+metadata.create_all(engine)
+
+conn = engine.connect()
